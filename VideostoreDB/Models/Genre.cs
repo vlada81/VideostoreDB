314aa22287e2d94dd2403e0aaeae8efc50a9d6e9
Vlada Krstin <krstinv@gmail.com>
2017-10-25 12:25:52 +0200

    Add - Models.Genre, kreirana Genre klasa

---
+++ VideostoreDB/Models/Genre.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideostoreDB.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Movie> Movies { get; set; }
+
+        public Genre()
+        {
+
+        }
+
+        public Genre(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
