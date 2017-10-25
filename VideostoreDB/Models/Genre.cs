8b8aa412f8e8280de513e87135df3a9bc37fdfd2
Vlada Krstin <krstinv@gmail.com>
2017-10-25 12:35:19 +0200

    Fix - Izbacena nepotrebna svojstva

--- VideostoreDB/Models/Genre.cs
+++ VideostoreDB/Models/Genre.cs
@@ -9,17 +9,6 @@ namespace VideostoreDB.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<Movie> Movies { get; set; }
 
-        public Genre()
-        {
-
-        }
-
-        public Genre(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
     }
 }
