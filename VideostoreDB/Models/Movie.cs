8bea3857b9f1b14a8d41389540c905d353d5669d
Vlada Krstin <krstinv@gmail.com>
2017-10-25 12:19:27 +0200

    Add - Models.Movie, kreirana klasa Movie

---
+++ VideostoreDB/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideostoreDB.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Genre Genre { get; set; }
+        public Boolean Deleted { get; set; }
+
+    }
+}
