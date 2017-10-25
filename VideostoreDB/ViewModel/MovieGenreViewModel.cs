7898fbd36efd261d99b54e6c657057e6502a7471
Vlada Krstin <krstinv@gmail.com>
2017-10-25 12:29:54 +0200

    Add - ViewModel.MoviGenreViewModel, kreirana klasa za ViewModel

---
+++ VideostoreDB/ViewModel/MovieGenreViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VideostoreDB.Models;
+
+namespace VideostoreDB.ViewModel
+{
+    public class MovieGenreViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Genre> Genres { get; set; }
+        public int SelectedGenreId { get; set; }
+    }
+}
